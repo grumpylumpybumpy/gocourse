51b798163fee198780849318b8f0767a35e390bc
grumpylumpybumpy <227109438+grumpylumpybumpy@users.noreply.github.com>
2025-08-26 01:10:53 +0700

    data types

--- basics/data.types.go
+++ basics/data.types.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello" + "Word")
+	fmt.Println("Hello" + "World")
 	fmt.Println("9 X 10 =", 9*10)
 	fmt.Println("180.18/2.0 = ", 180.18/2.0)
 
