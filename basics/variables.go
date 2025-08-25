b29fb832d269a6475eea790c47a17b6bc8df44bb
grumpylumpybumpy <227109438+grumpylumpybumpy@users.noreply.github.com>
2025-08-26 01:04:19 +0700

    Hello to NaminConv

---
+++ basics/variables.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+var middleName = "Cane" //collen equal is only used in function
+
+func main() {
+	//var age int
+	//var name string = "John"
+	//var name1 = "Jane"
+
+	//count := 10
+	//lastName := "Smith"
+
+	// middleName := "Mayor"
+	fmt.Println(middleName)
+	// Default values
+	// Numeric Types: 0
+	// Boolean Types: false
+	// String Type: " "
+	// Pointers, slices, maps, functions, and structs: nil
+
+	// ---- SCOPE
+	// fmt.Println(firstName)
+
+}
+
+func printname() {
+	firstname := "Michael"
+	fmt.Println(firstname)
+}
